cf9fd6a1f3e3c0c92f001169e64b8ab9e7594801
Deewansinghrajputs <deeprajput392@gmail.com>
2024-07-09 08:36:56 +0530

    foodi

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import './Header.css';
+
+const Header = () => {
+
+
+
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus natus cum, sed ipsum explicabo ex minus enim facere ipsam labore veritatis reprehenderit ut. Dolorem eius neque, quae excepturi veritatis voluptate!</p>
+            <button>View Menu</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
